fb446428fd43cd3eee871a1e81ea539a2f29d18b
romiyusnandar <yusromi04@gmail.com>
2026-08-04 12:23:51 +0700

    menambahkan file .gitignore dan implementasi server dengan endpoint API waktu untuk pengujian

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
+	"time"
 )
 
+//go:embed web
+var berkas embed.FS
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "halo dari server")
+	http.Handle("/", http.FileServer(http.FS(sub())))
+
+	http.HandleFunc("/api/waktu", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"waktu":%q}`, time.Now().Format("15:04:05"))
 	})
 
 	fmt.Println("buka http://localhost:1420")
-	http.ListenAndServe(":1420", nil)
+	http.ListenAndServe("127.0.0.1:1420", nil)
+}
+
+func sub() fs.FS {
+	f, err := fs.Sub(berkas, "web")
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
